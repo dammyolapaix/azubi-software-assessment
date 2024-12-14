aa9fb335d8d037916c11aeef6f4a8e5310218a6b
Dammy Ola <dammyolapaix@gmail.com>
2024-12-14 19:37:47 +0000

    Added StatusCode type to error instead of number for more type safe

--- src/api/v1/utils/errors.ts
+++ src/api/v1/utils/errors.ts
@@ -1,8 +1,10 @@
+type StatusCode = 200 | 201 | 400 | 401 | 403 | 500
+
 export class ErrorResponse extends Error {
   message: string
-  statusCode: number
+  statusCode: StatusCode
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: StatusCode) {
     super(message)
 
     this.message = message
