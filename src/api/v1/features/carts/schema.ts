81811a64ce32618dff07ee0d0776117bd0810f9c
Dammy Ola <dammyolapaix@gmail.com>
2024-12-15 19:50:17 +0000

    Added userId to cartItems schema

--- src/api/v1/features/carts/schema.ts
+++ src/api/v1/features/carts/schema.ts
@@ -2,12 +2,16 @@ import { relations } from 'drizzle-orm'
 import { integer, pgTable, uuid } from 'drizzle-orm/pg-core'
 import { timestamps } from '../../db/helper'
 import products from '../products/schema'
+import users from '../users/schema'
 
 const cartItems = pgTable('cart_items', {
   quantity: integer().default(1),
   productId: uuid()
     .notNull()
     .references(() => products.id),
+  userId: uuid()
+    .notNull()
+    .references(() => users.id),
   ...timestamps,
 })
 
@@ -15,6 +19,10 @@ export const cartItemsRelations = relations(cartItems, ({ one }) => ({
   product: one(products, {
     fields: [cartItems.productId],
     references: [products.id],
+  }),
+  user: one(users, {
+    fields: [cartItems.userId],
+    references: [users.id],
   }),
 }))
 
